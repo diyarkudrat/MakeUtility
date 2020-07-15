8afaad21b402a61fe33f7605f7d1d0334c702560
Diyar Kudrat <dkudrat@gmail.com>
2020-07-14 17:41:11 -0700

    stdout text from file and translated text

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/bregydoc/gtranslate"
 )
 
 func main() {
-	text := "Are you good?"
+	fileFlag := flag.String("file", "english-words.docx", "define input text")
+	flag.Parse()
+
+	// readFile(*fileFlag)
+	translateText(*fileFlag)
+}
+
+func translateText(fileName string) {
+	text := readFile(fileName)
 	translated, err := gtranslate.TranslateWithParams(
 		text,
 		gtranslate.TranslationParams{
@@ -21,4 +31,14 @@ func main() {
 
 	fmt.Printf("en: %s | ja: %s \n", text, translated)
 	// en: Hello World | ja: こんにちは世界
+}
+
+func readFile(fileName string) string {
+	fileContents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println(string(fileContents))
+	return string(fileContents)
 }
